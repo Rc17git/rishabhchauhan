5c0ef533cdae04a9e4b49354e123ce10ef51499f
Rishabh Chauhan <rishabh97.chauhan@gmail.com>
2022-12-28 15:38:10 +0530

    first deploy

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/Users/rishabhchauhan/rishabhchauhan/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
+  "component---src-pages-404-jsx": preferDefault(require("/Users/rishabhchauhan/rishabhchauhan/src/pages/404.jsx"))
+}
+
